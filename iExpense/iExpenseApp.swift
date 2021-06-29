18a6118fe312e2a4dfaa55146a1d19bc94b434c9
Iresharma <iersh.sharma8@gmail.com>
2021-06-30 00:57:39 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by iresh sharma on 30/06/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
